4419a519e4f96e078de0f33b57e899edc537d2b4
dhruvanayan <100090755+dhruvanayan@users.noreply.github.com>
2022-12-23 19:09:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function TV() {
+    window.location="TV.html";
+}
+
+
+function Pencil() {
+    window.location="Pencil.html";
+}
+
+function WaterBottle() {
+    window.location="WaterBottle.html";
+}
+
+function Chair() {
+    window.location="Chair.html";
+}
+
+function Iphone() {
+    window.location="Iphone.html";
+}
+
